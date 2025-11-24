b854a7dde6b4a90b92209cf367e8f024de67a5b5
Erik Haba Molino <erikgalamolino@gmail.com>
2025-11-24 22:35:17 +0000

    dim_match

--- models/marts/fct_player_map_performance.sql
+++ models/marts/fct_player_map_performance.sql
@@ -8,7 +8,7 @@ with stats as (
 ),
 
 matches as (
-    select match_pk, event_id, match_id, match_date, stage_id, format_id
+    select match_pk, event_id, match_id
     from {{ ref('stg_match') }}
 ),
 
@@ -17,16 +17,13 @@ directory as (
 )
 
 select
+
     {{ dbt_utils.generate_surrogate_key(['s.player_match_map_stats_pk']) }} as player_performance_sk,
-        s.event_id as event_fk,
-    m.match_pk as match_fk,
+    {{ dbt_utils.generate_surrogate_key(['s.event_id']) }} as event_fk,
     {{ dbt_utils.generate_surrogate_key(['s.map_id']) }} as map_fk,
     {{ dbt_utils.generate_surrogate_key(['s.player_id']) }} as player_fk,
-
     d.team_fk as team_fk,
-    m.match_date,
-    m.stage_id,
-    m.format_id,
+    m.match_pk as match_fk,
     s.k as kills,
     s.d as deaths,
     s.a as assists,
@@ -39,6 +36,7 @@ select
     s.fk as first_kills,
     s.fd as first_deaths,
     s.fk_fd_diff as first_kill_diff
+
 from stats s
 join matches m 
     on s.match_id = m.match_id 
